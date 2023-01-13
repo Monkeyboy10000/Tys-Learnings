c0a4b55ccd64e628119a5088b2ce21a97486473b
Ty <89813366+Monkeyboy10000@users.noreply.github.com>
2023-01-13 10:31:05 -0600

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,1 +1,10 @@
-print(dog)
+#Basics
+
+import time #stuff life time.sleep(#) 
+
+Men = ['a', 'b', 'c']
+
+for Mens in Men:
+    print(Mens) #to loop untill finished (**need the print(thing) and (for thing) to be the same
+    
+\n #will move to a new line when printing print("Hey all,", "Scott here\n")
