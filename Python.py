8cbf9b23ce58e5df03f9b356c35066b982587c40
Ty <89813366+Monkeyboy10000@users.noreply.github.com>
2023-01-19 10:03:02 -0600

    Update Python.py

--- Python.py
+++ Python.py
@@ -8,3 +8,10 @@ for Mens in Men:
     print(Mens) #to loop untill finished (**need the print(thing) and (for thing) to be the same
     
 \n #will move to a new line when printing print("Hey all,", "Scott here\n")
+
+dogs = ['dog1', 'dog2', 'dog3']
+dogs[0] = 'big dog'
+(replaces number in list with specified *thing*)
+
+print(dogs.index('dog1'))
+#finds which number specified *thing* is at
