70d24190272aedc268179e6b883849b55bef8450
Ty <89813366+Monkeyboy10000@users.noreply.github.com>
2023-01-19 10:20:33 -0600

    Update Python.py

--- Python.py
+++ Python.py
@@ -4,14 +4,24 @@ import time #stuff life time.sleep(#)
 
 Men = ['a', 'b', 'c']
 
+print(men.title()) #will make this into This
+
 for Mens in Men:
     print(Mens) #to loop untill finished (**need the print(thing) and (for thing) to be the same
     
-\n #will move to a new line when printing print("Hey all,", "Scott here\n")
+\n #will move to a new line when printing print("Hey all,", "\nScott here")
 
 dogs = ['dog1', 'dog2', 'dog3']
+
 dogs[0] = 'big dog'
-(replaces number in list with specified *thing*)
+#replaces number in list with specified *thing*
 
 print(dogs.index('dog1'))
 #finds which number specified *thing* is at
+
+print(dog1 in dogs)
+#true or false if item is in list (this would be true)
+
+dogs.append('dog3')
+#adds *thing* to end of the list
+
