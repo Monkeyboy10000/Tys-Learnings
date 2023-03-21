c1f2caa8ae48a925c3f72cc06b0c7b9017ab941a
Ty <89813366+Togradoty@users.noreply.github.com>
2023-03-20 22:02:37 -0500

    Update Python.py

--- Python.py
+++ Python.py
@@ -87,6 +87,15 @@ import sys
 #figure it out yourself, idc
 if sys.platform.startswith('linux'):
       print("yippe")
+    
+
+
+num1 = 1_000_000_000
+
+num2 = 2_000
+
+ans = num1*num2
+print(f'{ans:_}')
 
       
       
